4b071cd3294962e79903236f9d287a532021f9a9
Marco <mt.marco87@gmail.com>
2019-10-24 15:43:08 +0200

    RunAll changed

--- runall.bat
+++ runall.bat
@@ -3,7 +3,11 @@ echo Activating Scrapy Conda environment
 CALL C:\DevTools\Anaconda3\condabin\conda.bat activate bet_parser_3.6
 echo Launching Scrapy Spider for: Bet365, Bwin, William Hill and Sisal
 cd C:/DevTools/Projects/bet_parser/
-start "" scrapy crawl b365
-start "" scrapy crawl bwin
-start "" scrapy crawl william
-start "" scrapy crawl sis
+start "" cmd /c scrapy crawl b365 
+echo ^> logB365.txt
+start "" cmd /c scrapy crawl bwin 
+echo ^> logBwin.txt
+start "" cmd /c scrapy crawl william 
+echo ^> logWilliam.txt
+start "" cmd /c scrapy crawl sis 
+echo ^> logSis.txt
