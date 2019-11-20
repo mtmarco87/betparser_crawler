961a81e18af0d60f5639b2c94a6214706ed52564
Marco <mt.marco87@gmail.com>
2019-11-20 23:44:16 +0100

    Latest Updates

--- runall.bat
+++ runall.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Activating Scrapy Conda environment
-CALL C:\DevTools\Anaconda3\condabin\conda.bat activate bet_parser_3.6
+CALL C:\yourcondadir\Anaconda3\condabin\conda.bat activate yourcondaenv
 echo Launching Scrapy Spider for: Bet365, Bwin, William Hill and Sisal
-cd C:/DevTools/Projects/betparser_crawler/
+cd C:/your_betparser_crawler_dir/betparser_crawler/
 start "" scrapy crawl b365 
 start "" scrapy crawl bwin
 start "" scrapy crawl william
